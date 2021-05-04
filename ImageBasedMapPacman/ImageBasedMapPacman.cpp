cbaa6090b4d02cb38431215be22fa4528a4f2493
zoown13 <41746120+zoown13@users.noreply.github.com>
2021-05-04 22:06:13 +0900

    이미지 경로 절대 경로에서 상대경로로 변경 및 창크기 Default 크기로 조정

--- ImageBasedMapPacman/ImageBasedMapPacman.cpp
+++ ImageBasedMapPacman/ImageBasedMapPacman.cpp
@@ -98,7 +98,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, 1920, 1080, nullptr, nullptr, hInstance, nullptr);
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
